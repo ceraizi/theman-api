a684da43d2c5ded423438a68cd424bcc9428d39b
Martim Neves <168484198+ceraizi@users.noreply.github.com>
2026-02-09 12:58:18 +0000

    update and delete tasks

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -19,3 +19,37 @@ export const createTask = (req: Request, res: Response) => {
   tasks.push(newTask);
   res.status(201).json(newTask);
 };
+
+export const deleteTask = (req: Request, res: Response) => {
+  const {id} = req.params;
+  
+  const originalLength = tasks.length;
+  tasks = tasks.filter(t => t.id !== parseInt(id));
+
+  if (tasks.length === originalLength) {
+    return res.status(404).json({message: "Task not found"});
+  }
+
+  res.json({message: "Task deleted successfully"});
+};
+
+export const updateTask = (req: Request, res: Response) => {
+  const {id} = req.params;
+  const {title, completed} = req.body;
+
+  const taskIndex = tasks.findIndex(t => t.id === parseInt(id));
+
+  if (taskIndex === -1) {
+    return res.status(404).json({ message: "Task not found" });
+  }
+
+  if (title !== undefined){
+    tasks[taskIndex].title = title;
+  }
+  
+  if (completed !== undefined){
+    tasks[taskIndex].completed = completed;
+  }
+
+  res.json(tasks[taskIndex]);
+};
--- src/routes/TaskRoutes.ts
+++ src/routes/TaskRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getTasks, createTask } from '../controllers/taskController';
+import { getTasks, createTask, deleteTask, updateTask } from '../controllers/taskController';
 
 const router = Router();
 
 router.get('/', getTasks);
 router.post('/', createTask);
+router.put('/:id', updateTask);
+router.delete('/:id', deleteTask);
 
 export default router;
